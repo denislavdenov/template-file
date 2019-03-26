0e221e30c4b76095f597c7bcea378720843b7a9e
Denov <denov@Denovs-MBP.zyxel.com>
2019-03-26 14:28:39 +0200

    adding changes

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
+# This code assume you have your own ssh key, and you want to use that to connect to the new ami
 resource "aws_key_pair" "key" {
   key_name   = "key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
+# This code is used to provide terraform script with variables.
 data "template_file" "var" {
-  template   = "${file("${path.module}/scripts/temp.sh")}"
+  template   = "${file("${path.module}/scripts/temp.tpl")}"
   depends_on = ["aws_key_pair.key"]
 
   vars = {
@@ -14,28 +16,31 @@ data "template_file" "var" {
   }
 }
 
+# Code below creates an AMI and provisions it with the script created using the template-file data source
 resource "aws_instance" "temp1" {
-  ami                         = "${var.ami}"
-  instance_type               = "${var.instance_type}"
-  subnet_id                   = "${var.subnet_id}"
-  key_name                    = "${aws_key_pair.key.id}"
-  vpc_security_group_ids      = "${var.security_group_id}"
-  
+  ami                    = "${var.ami}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = "${var.subnet_id}"
+  key_name               = "${aws_key_pair.key.id}"
+  vpc_security_group_ids = "${var.security_group_id}"
+
   connection {
     user        = "ubuntu"
     private_key = "${file("~/.ssh/id_rsa")}"
   }
 
+  provisioner "file" {
+    content     = "${data.template_file.var.rendered}"
+    destination = "/var/tmp/temp.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "cat <<EOT > /var/tmp/temp.sh",
-      "${data.template_file.var.rendered}",
-      "EOT",
-      "sudo bash /var/tmp/temp.sh"
-      
+      "sudo bash /var/tmp/temp.sh",
     ]
   }
 }
+
 output "IP" {
   value = "${aws_instance.temp1.public_ip}"
 }
