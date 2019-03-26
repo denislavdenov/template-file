6e156cd509c12aa70b35bc322aac670d09f06878
Denov <denov@Denovs-MBP.zyxel.com>
2019-03-26 16:41:28 +0200

    editing text

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Fork and clone the repo.
 
 Create a `terraform.tfvars` file and fill in a value for all the variables in `variables.tf` file.
 
-When done, do `terraform apply`.
+When done, do `terraform plan` and then `terraform apply`.
 
 In `/var/tmp/` on the server that is created should be text file `temp.txt` with a string `Hello content of var1 content of var2 content of var3!`
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,38 @@
-variable "aws_access_key" {}
+variable "aws_access_key" {
+  description = "Your AWS access key"
+}
 
-variable "aws_secret_key" {}
+variable "aws_secret_key" {
+  description = "Your AWS secret key"
+}
 
-variable "instance_type" {}
+variable "instance_type" {
+  description = "Specify the preferred instance type"
+}
 
-variable "subnet_id" {}
+variable "subnet_id" {
+  description = "Specify the preferred subnet"
+}
 
-variable "region" {}
+variable "region" {
+  description = "Specify the prefered AWS region"
+}
 
 variable "security_group_id" {
   type = "list"
+  description = "Specify the prefered AWS security group"
 }
 
-variable "ami" {}
+variable "ami" {
+  description = "Specify the prefered AWS AMI id"
+}
 
-variable "var1" {}
-variable "var2" {}
-variable "var3" {}
+variable "var1" {
+  description = "String that will be interpolated inside a script that template-file creates"
+}
+variable "var2" {
+  description = "String that will be interpolated inside a script that template-file creates"
+}
+variable "var3" {
+  description = "String that will be interpolated inside a script that template-file creates"
+}
